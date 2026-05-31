6ce5db4c193bacf7bb00ac1cbbed0f19b89e36c9
dgsuma <dumindags@gmail.com>
2026-05-31 16:23:31 +1000

    chore: libraries added to the ScrapeIndeedEvomiScraperAPI.py file

--- ScrapeIndeedEvomiScraperAPI.py
+++ ScrapeIndeedEvomiScraperAPI.py
@@ -1,2 +1,23 @@
+import os
+import re
+import json
+import requests # type: ignore
+from bs4 import BeautifulSoup #
+from datetime import datetime
+from dotenv import load_dotenv # type: ignore
 
+# Load environment variables from .env file
+load_dotenv()
 
+def display_jobs_clean(jobs):
+    if not jobs:
+        print("\n[!] No jobs found to display. \n")
+        return
+    
+    # Define column widths
+    t_width = 30 # Title
+    c_width = 20 # Company
+    l_width = 20 # Location
+    s_width = 15 # Salary
+    p_width = 12 # Posted Date
+    
