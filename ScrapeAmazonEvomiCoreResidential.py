2f6fcfc08a68f9da5701b94f8f8a45c705a43cad
dgsuma <dumindags@gmail.com>
2026-06-01 19:37:33 +1000

    chore: libraries added to the ScrapeAmazonEvomiCoreResidential.py file

---
+++ ScrapeAmazonEvomiCoreResidential.py
@@ -0,0 +1,6 @@
+import requests  # type: ignore # pip install requests
+from bs4 import BeautifulSoup  # pip install beautifulsoup4
+import os
+import re
+from dotenv import load_dotenv  # type: ignore # pip install python-dotenv
+
